75199c16a82befdaf0949147dc916f41cc1e7818
unknown <onurcoskun99@gmail.com>
2020-11-11 14:48:12 +0300

    expense component done

--- client/src/components/expense/expense.tsx
+++ client/src/components/expense/expense.tsx
@@ -1,28 +1,16 @@
-import* as React from 'react';
-import axios from 'axios';
+import * as React from 'react';
+import { userContext } from '../../store/context/userContext';
+import List from '../list/list';
+import Header from '../header/header';
 
-const Expense:React.FC=()=>{
-
-    const [expense,setExpense]=React.useState<string>('name your expense');
-    const [price,setPrice]=React.useState<number>(0);
-    const [frequency,setFrequency]=React.useState<string>('')
-    const [date,setDate]=React.useState<string>('today');
-
-    const addHandler=(e:React.MouseEvent<HTMLElement>)=>{
-        e.preventDefault();
-        const payload={name:expense,price,frequency,date};
-        axios.post('http://localhost:8000/api/expense/',payload).then(res=>console.log(res.data)).catch(err=>console.log(err))
-        console.log('add expense')
-        console.table(payload)
-
-    }
-    return <div>
-        <input type="text" value={expense} onChange={(e)=>{setExpense(e.target.value)}}/>
-        <input type="text" value={price} onChange={(e)=>{setPrice(+e.target.value)}}/>
-        <input type="text"value={frequency} onChange={e=>setFrequency(e.target.value)}/>
-        <input type="text" value={date} onChange={e=>setDate(e.target.value)}/>
-        <button onClick={addHandler}>add Expense</button>
-    </div>
-}
+const Expense: React.FC = () => {
+    const { state } = React.useContext(userContext);
+    return (
+        <div className="expenses">
+            <Header />
+            <List expenses={state.expenses.expenses} />
+        </div>
+    );
+};
 
 export default Expense;
