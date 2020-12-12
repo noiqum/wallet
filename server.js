b9484516ddbceeb336bd3a921318f27df02ef7b2
unknown <onurcoskun99@gmail.com>
2020-12-13 01:26:33 +0300

    delete cors

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const cors = require('cors');
+
 const mongoose = require('mongoose');
 const routes = require('./app/routes/index');
 require('dotenv').config();
@@ -12,7 +12,6 @@ const app = express();
 const port = process.env.PORT || 8000;
 
 
-app.use(cors());
 app.use(express.json())
 app.use(routes)
 app.use(cookieParser())
